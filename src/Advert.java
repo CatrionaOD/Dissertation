4921c824c01befe6ac9a58bb05e3192b68ea7fca
Catriona O Driscoll <odriscc4@tcd.ie>
2019-02-05 01:19:58 +0000

    temperature based decisions being made, starting to work on ticketmaster api

--- src/Advert.java
+++ src/Advert.java
@@ -4,14 +4,16 @@ public class Advert {
 	public String adSeason;
 	public String imageSrc;
 	public String adWeather;
+	public String adTemp;
 
-	public Advert(String seasonIn, String weatherIn, String imagePath) {
+	public Advert(String seasonIn, String weatherIn, String tempIn, String imagePath) {
 //		if ((seasonIn.equals("spring"))||(seasonIn.equals("summer"))||(seasonIn.equals("autumn"))||(seasonIn.equals("winter"))) {
 			adSeason = seasonIn;
 //		}
 		
 		adWeather = weatherIn;
 		imageSrc = imagePath;
+		adTemp = tempIn;
 	}
 	
 
--- src/mainClass.java
+++ src/mainClass.java
@@ -44,21 +44,10 @@ public class mainClass {
 
 	public static void main(String[] args) throws IOException, JSONException {
 		
-		String[][] seasonArray = new String[4][4];
-		
-//		seasonArray[0][0] = "Winter.jpg";
-//		seasonArray[0][1] = "winter";
-//		seasonArray[1][0] = "Spring.jpg";
-//		seasonArray[1][1] = "spring";
-//		seasonArray[2][0] = "Summer.jpg";
-//		seasonArray[2][1] = "summer";
-//		seasonArray[3][0] = "Autumn.jpg";
-//		seasonArray[3][1] = "autumn";
-		
-		Advert winterAd = new Advert("winter", "", "Winter.jpg");
-		Advert springAd = new Advert("spring", "", "Spring.jpg");
-		Advert summerAd = new Advert("summer", "", "Summer.jpg");
-		Advert autumnAd = new Advert("autumn", "", "Autumn.jpg");
+		Advert winterAd = new Advert("winter", "", "", "Winter.jpg");
+		Advert springAd = new Advert("spring", "", "", "Spring.jpg");
+		Advert summerAd = new Advert("summer", "", "", "Summer.jpg");
+		Advert autumnAd = new Advert("autumn", "", "", "Autumn.jpg");
 		
 		ArrayList<Advert> advertArray = new ArrayList<Advert>();
 		advertArray.add(summerAd);
@@ -91,26 +80,16 @@ public class mainClass {
 				path = advertArray.get(i).imageSrc;
 			}
 		}
-
-		//// Print out image given path
-//		 BufferedImage img=ImageIO.read(new File(path));
-//	     ImageIcon icon=new ImageIcon(img);
-//	     JFrame frame=new JFrame();
-//	     frame.setLayout(new FlowLayout());
-//	     frame.setSize(200,300);
-//	     JLabel lbl=new JLabel();
-//	     lbl.setIcon(icon);
-//	     frame.add(lbl);
-//	     frame.setVisible(true);
-//	     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	     
+//		showAd (path);
 	     	
-		String API_KEY = "131f84a6effc0274c8e2b0260bb9dc26";
+		String WEATHER_API_KEY = "131f84a6effc0274c8e2b0260bb9dc26";
 		String LOCATION = "Dublin, IE";
-		String urlString = "http://api.openweathermap.org/data/2.5/weather?q="+LOCATION+"&appid="+API_KEY+"&units=metric";
+		String urlString = "http://api.openweathermap.org/data/2.5/weather?q="+LOCATION+"&appid="+WEATHER_API_KEY+"&units=metric";
 		
             String retVal = "";
             String weatherType = "";
+            double currTemp = 0;
+            String tempType = "";
 
             try {
                 URL url = new URL(urlString);
@@ -124,7 +103,7 @@ public class mainClass {
                     data = reader.read();
                 }
 
-                System.out.println("retVal: "+retVal);
+//                System.out.println("retVal: "+retVal);
             } catch (Exception e) {
             	System.out.println(e);
             }
@@ -139,22 +118,23 @@ public class mainClass {
 //                    System.out.println("weatherType "+weatherType);
 //                  System.out.println("descText "+descText);
                     
-//                  Map<String, Object> respMap = jsonToMap(retVal.toString());
-//        			Map<String, Object> mainMap = jsonToMap(respMap.get("main").toString());
+                    Map<String, Object> respMap = jsonToMap(retVal.toString());
+        			Map<String, Object> mainMap = jsonToMap(respMap.get("main").toString());
 
-//        			System.out.println("Curr Temp: "+mainMap.get("temp"));
+        			currTemp =  (double) mainMap.get("temp");
+//        			System.out.println("Curr Temp: "+currTemp);
                 }
             } catch (JSONException e) {
                 e.printStackTrace();
             }
         
             
-            Advert clearAd = new Advert("", "Clear", "Clear.jpg");
-    		Advert cloudsAd = new Advert("", "Clouds", "Clouds.jpg");
-    		Advert drizzleAd = new Advert("", "Drizzle", "Drizzle.jpg");
-    		Advert rainAd = new Advert("", "Rain", "Rain.jpg");
-    		Advert snowAd = new Advert("", "Snow", "Snow.jpg");
-    		Advert tstormAd = new Advert("", "Thunderstorm", "Thunderstorm.jpg");
+            Advert clearAd = new Advert("", "Clear", "", "Clear.jpg");
+    		Advert cloudsAd = new Advert("", "Clouds", "", "Clouds.jpg");
+    		Advert drizzleAd = new Advert("", "Drizzle", "", "Drizzle.jpg");
+    		Advert rainAd = new Advert("", "Rain", "", "Rain.jpg");
+    		Advert snowAd = new Advert("", "Snow", "", "Snow.jpg");
+    		Advert tstormAd = new Advert("", "Thunderstorm", "", "Thunderstorm.jpg");
     		
     		advertArray.add(clearAd);
     		advertArray.add(cloudsAd);
@@ -170,30 +150,92 @@ public class mainClass {
 //    				System.out.println("OUT "+advertArray.get(i).imageSrc);
     			}
     		}
-
-    		//// Print out image given path
-    		 BufferedImage img=ImageIO.read(new File(path));
-    	     ImageIcon icon=new ImageIcon(img);
-    	     JFrame frame=new JFrame();
-    	     frame.setLayout(new FlowLayout());
-    	     frame.setSize(200,300);
-    	     JLabel lbl=new JLabel();
-    	     lbl.setIcon(icon);
-    	     frame.add(lbl);
-    	     frame.setVisible(true);
-    	     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//    		showAd (path);
     	     
+			if ((-30 <= currTemp)&&(currTemp <= -10)) {tempType = "Extremely Freezing";}
+			else if ((-10 < currTemp)&&(currTemp <= 0)) {tempType = "Freezing";} 
+			else if ((0 < currTemp)&&(currTemp <= 10)) {tempType = "Very Cold";} 
+			else if ((10 < currTemp)&&(currTemp <= 15)) {tempType = "Cold";} 
+			else if ((15 < currTemp)&&(currTemp <= 20)) {tempType = "Cool";} 
+			else if ((20 < currTemp)&&(currTemp <= 25)) {tempType = "Warm";} 
+			else if ((25 < currTemp)&&(currTemp <= 30)) {tempType = "Very Warm";} 
+			else if ((30 < currTemp)&&(currTemp <= 37)) {tempType = "Hot";} 
+			else if ((37 < currTemp)&&(currTemp <= 50)) {tempType = "Very Hot";} 
+			else if (50 < currTemp) {tempType = "Extremely Hot";} 
 			
+			Advert extremelyFreezingAd = new Advert("", "", "Extremely Freezing", "ExtremelyFreezing.jpg");
+			Advert freezingAd = new Advert("", "", "Freezing", "Freezing.jpg");
+			Advert veryColdAd = new Advert("", "", "Very Cold", "VeryCold.jpg");
+			Advert coldAd = new Advert("", "", "Cold", "Cold.jpg");
+			Advert coolAd = new Advert("", "", "Cool", "Cool.jpg");
+			Advert warmAd = new Advert("", "", "Warm", "Warm.jpg");
+			Advert veryWarmAd = new Advert("", "", "Very Warm", "VeryWarm.jpg");
+			Advert hotAd = new Advert("", "", "Hot", "Hot.jpg");
+			Advert veryHotAd = new Advert("", "", "Very Hot", "VeryHot.jpg");
+			Advert ExtremelyHotAd = new Advert("", "", "Extremely Hot", "ExtremelyHot.jpg");
 			
-		
+			advertArray.add(extremelyFreezingAd);
+			advertArray.add(freezingAd);
+			advertArray.add(veryColdAd);
+			advertArray.add(coldAd);
+			advertArray.add(coolAd);
+			advertArray.add(warmAd);
+			advertArray.add(veryWarmAd);
+			advertArray.add(hotAd);
+			advertArray.add(veryHotAd);
+			advertArray.add(ExtremelyHotAd);
+			
+			for (int i=0; i<advertArray.size(); i++) {
+//    			System.out.println("OUT1 "+advertArray.get(i).adTemp);
+    			if (advertArray.get(i).adTemp.equals(tempType)) {
+    				path = advertArray.get(i).imageSrc;
+//    				System.out.println("OUT "+advertArray.get(i).imageSrc);
+    			}
+    		}
+//			showAd (path);
+			
+//			String EVENTS_API_KEY = "131f84a6effc0274c8e2b0260bb9dc26";
+//			String EVENTS_LOCATION = "Dublin, IE";
+//			String eventsUrlString = "https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=Qn3VKm3dgWtS0AXLUiQ5zEIorM54bTli";
+////			String eventsUrlString = "https://api.predicthq.com/v1/events/count/?country=IE&apikey=phq.T9og95XRJGYHLmDBBte4KRo3lBYsxCU8mJmrtLDQ";
+//			
+//			String ticketMasterReturn = "";
+//			try {
+//                URL url = new URL(eventsUrlString);
+//                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+//                InputStream in = conn.getInputStream();
+//                InputStreamReader reader = new InputStreamReader(in);
+//                int data = reader.read();
+//                while (data != -1) {
+//                    char c = (char) data;
+//                    ticketMasterReturn += c;
+//                    data = reader.read();
+//                }
+//
+//                System.out.println("ticketmaster: "+ticketMasterReturn);
+//            } catch (Exception e) {
+//            	System.out.println(e);
+//            }
 	}
-	
 	
 	// Convert JSON to Map
 	public static Map<String, Object> jsonToMap(String str) {
 		Map<String, Object> map = new Gson().fromJson(
 				str, new TypeToken<HashMap<String, Object>>() {}.getType());
 		return map;
+	}
+	
+	public static void showAd (String pathIn) throws IOException {
+		BufferedImage img=ImageIO.read(new File(pathIn));
+  	     ImageIcon icon=new ImageIcon(img);
+  	     JFrame frame=new JFrame();
+  	     frame.setLayout(new FlowLayout());
+  	     frame.setSize(200,300);
+  	     JLabel lbl=new JLabel();
+  	     lbl.setIcon(icon);
+  	     frame.add(lbl);
+  	     frame.setVisible(true);
+  	     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
